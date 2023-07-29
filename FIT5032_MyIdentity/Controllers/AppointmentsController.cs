09b7f14e23d84d34c8c54c6d896af04082f08463
caiwentao <2039994970@qq.com>
2023-07-29 16:53:46 +0800

    first commit

---
+++ FIT5032_MyIdentity/Controllers/AppointmentsController.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032_MyIdentity.Models;
+
+namespace FIT5032_MyIdentity.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        private FIT5032_Models db = new FIT5032_Models();
+
+        // GET: Appointments
+        public ActionResult Index()
+        {
+            return View(db.Appointments.ToList());
+        }
+
+        // GET: Appointments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointments appointments = db.Appointments.Find(id);
+            if (appointments == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appointments);
+        }
+
+        // GET: Appointments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        /*
+        // POST: Appointments/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性。有关
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,StudentID,EngineerID,Date")] Appointments appointments)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Appointments.Add(appointments);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(appointments);
+        }
+        */
+        [HttpPost]
+        public String CreateAppointment()
+        {
+
+
+            var appointment = new Appointments
+            {
+                StudentID = int.Parse(Request.Form["StudentID"]),
+                EngineerID = int.Parse(Request.Form["EngineerID"]),
+                Date = Request.Form["AppointmentDate"]
+            };
+
+            var vx = Request.Files["Attachment"].ContentLength;
+
+            // Store the attachment in local storage.
+            var Str1 = Request.Files[0].FileName.Split('.');
+            var FileType = Str1[Str1.Length - 1];
+            var FilePath =
+                Server.MapPath("~/Uploads/") +
+                string.Format(@"{0}", Guid.NewGuid()) +
+                "." + FileType;
+            Request.Files[0].SaveAs(FilePath);
+
+               
+
+
+
+
+
+            if (ModelState.IsValid)
+            {
+                // Add the appointment into the database.
+                db.Appointments.Add(appointment);
+                db.SaveChanges();
+
+                // Send confirmation email.
+                var mail = new MailMessage();
+                mail.To.Add(new MailAddress(Request.Form["EmailAddress"]));
+                mail.From = new MailAddress("lokii0131@outlook.com");
+
+                mail.Subject = "Appointment Conformation";
+                mail.Body =
+                    "You made an appointment:\n" +
+                    "Student ID: " + Request.Form["StudentID"] + "\n" +
+                    "Engineer ID: " + Request.Form["EngineerID"] + "\n" +
+                    "Date: " + Request.Form["AppointmentDate"];
+                mail.IsBodyHtml = false;
+
+                var attachment = new System.Net.Mail.Attachment(FilePath);
+                mail.Attachments.Add(attachment);
+
+                var smtp = new SmtpClient();
+                smtp.Host = "smtp.office365.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.Credentials = new System.Net.NetworkCredential
+                    ("lokii0131@outlook.com", "sueg6y51");
+
+                smtp.Send(mail);
+
+
+
+                return "Success";
+            }
+
+            return "Database Unavailable.";
+        }
+
+
+        // GET: Appointments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointments appointments = db.Appointments.Find(id);
+            if (appointments == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appointments);
+        }
+
+        // POST: Appointments/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性。有关
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,StudentID,EngineerID,Date")] Appointments appointments)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(appointments).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(appointments);
+        }
+
+        // GET: Appointments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointments appointments = db.Appointments.Find(id);
+            if (appointments == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appointments);
+        }
+
+        // POST: Appointments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Appointments appointments = db.Appointments.Find(id);
+            db.Appointments.Remove(appointments);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SQLQuery2.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Appointments](
+	[Id] int IDENTITY(1,1) NOT NULL,
+	[StudentID] int NOT NULL,
+	[EngineerID] int NOT NULL,
+	[Date] nvarchar(max) NOT NULL,
+	PRIMARY KEY (Id)
+);
